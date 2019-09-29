1af5622c35734457acffe57af4608b3ce8f8241c
Brian Collins <collins.brian@gmail.com>
2019-09-28 22:32:51 -0700

    added a results page, added subdir

--- flask_version/main.py
+++ flask_version/main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 from werkzeug import secure_filename
 import os
+import random
+import string
 
 app = Flask(__name__)
 output_path = 'uploads'
@@ -17,10 +19,13 @@ def upload_file():
 def file_uploaded():
    if request.method == 'POST':
       f = request.files['file']
-      if not os.path.exists(output_path):
-        os.makedirs(output_path)
-
-      f.save(output_path + os.sep + secure_filename(f.filename))
+      id = ''.join([random.choice(string.ascii_letters 
+         + string.digits) for n in range(13)]) 
+      folder = output_path + '/' + id + '_open/subdir/'
+      if not os.path.exists(folder):
+        os.makedirs(folder)
+        
+      f.save(folder + secure_filename(f.filename))
       return render_template('results.html')
 		
 if __name__ == '__main__':
