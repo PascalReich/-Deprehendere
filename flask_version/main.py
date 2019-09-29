8fae2c38b8e51ed8d2d3932526a4472ea6b6d760
22preich <44716533+22preich@users.noreply.github.com>
2019-09-29 00:58:47 -0700

    dsfd

--- flask_version/main.py
+++ flask_version/main.py
@@ -3,30 +3,34 @@ from werkzeug import secure_filename
 import os
 import random
 import string
+from SE_code.main import main as run
 
 app = Flask(__name__)
 output_path = 'uploads'
 
+
 @app.route('/')
 def main():
-   return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/upload')
 def upload_file():
-   return render_template('upload.html')
-	
-@app.route('/uploader', methods = ['GET', 'POST'])
+    return render_template('upload.html')
+
+
+@app.route('/uploader', methods=['GET', 'POST'])
 def file_uploaded():
-   if request.method == 'POST':
-      f = request.files['file']
-      id = ''.join([random.choice(string.ascii_letters 
-         + string.digits) for n in range(13)]) 
-      folder = output_path + '/' + id + '_open/subdir/'
-      if not os.path.exists(folder):
-        os.makedirs(folder)
-        
-      f.save(folder + secure_filename(f.filename))
-      return render_template('results.html')
-		
+    if request.method == 'POST':
+        f = request.files['file']
+        id = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(13)])
+        folder = output_path + '/' + id + '_open/subdir/'
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+
+        f.save(folder + secure_filename(f.filename))
+    return render_template('results.html')
+
+
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug=True)
