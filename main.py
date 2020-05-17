6876e6b2cb94e72eaf540a60bea93bbe77a35eed
22preich <44716533+22preich@users.noreply.github.com>
2020-05-16 19:36:09 -0700

    reformating

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 from flask import Flask, render_template, request
-from werkzeug.utils import secure_filename
-import os
-import random
-import string
 import predict
 import cropper
 import time
 import base64
 
 app = Flask(__name__)
-output_path = 'temp/'
 
 
 @app.route('/')
@@ -21,20 +16,26 @@ def main():
 def upload_file():
     return render_template('upload.html')
 
+
+"""
 @app.route('/src/<folder>/<img>')
 def serve(folder, img):
     app.send_file('src/' + folder + '/' + img)
+"""
 
-@app.route('/uploader', methods=['GET', 'POST'])
+
+@app.route('/uploader', methods=['POST'])
 def file_uploaded():
     if request.method == 'POST':
         f = request.files['file']
         start = time.time()
         img, base64compat = cropper.cropFileStorageObject(f)
         labels = predict.predictFromTensor(img)
-    else: raise TypeError
-        
-    return render_template('results.html', labels=labels, base64=base64.b64encode(base64compat).decode(), time=time.time()-start)
+    else:
+        raise TypeError
+
+    return render_template('results.html', labels=labels, base64=base64.b64encode(base64compat).decode(),
+                           time=time.time() - start)
 
 
 if __name__ == '__main__':
--- predict.py
+++ predict.py
@@ -1,23 +1,11 @@
 import tensorflow as tf
 import numpy as np
-# import time
-# from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import json, os
 
-"""
-testdatagen = ImageDataGenerator(rescale=1./255)
-
-# test_it = testdatagen.flow_from_directory('dbs/test', shuffle=False, target_size=(80, 80), class_mode='binary')
-# print(test_it.next()[0].shape)
-test_it = testdatagen.flow_from_directory('dbs/lfw-cropped', shuffle=False, target_size=(80, 80), class_mode='binary')
-
-labels = dict((v,k) for k,v in test_it.class_indices.items())
-"""
 models = {
     "id": tf.keras.models.load_model("models/id/idk-id.h5"),
     "gen": tf.keras.models.load_model("models/gen/15-altb-gen.h5"),
     "age": tf.keras.models.load_model("models/age/idk-long-age.h5")
-
 }
 
 
@@ -29,9 +17,8 @@ def get_indices(name):
 
 def get_label(name, model, img):
     pred = model.predict_on_batch(tf.expand_dims(img, 0))
-    # print(get_indices("id")[str(np.argmax(pred))])
     if name in ("id", "age"):
-        return get_indices(name)[str(np.argmax(pred))], 0  # pred[0][np.argmax(pred)].numpy()
+        return get_indices(name)[str(np.argmax(pred))], 0
     elif name == "gen":
         return get_indices("gen")[str(int(round(pred[0][0].numpy())))], pred[0][0].numpy() if not int(
             round(pred[0][0].numpy())) else 100 - pred[0][0].numpy()
@@ -60,11 +47,8 @@ def predictFromPath(img_path="dbs/test/Ariel_Sharon_0006.jpg"):
         }
     }
 
-    # print(image.shape)
-
     for mod in models.keys():
         labels = get_label(mod, models[mod], image)
-        # print(labels[0], f"{labels[1] * 100}%")
         returns[mod]["class"], returns[mod]["confidence"] = labels[0], f"{labels[1] * 100}%"
 
     return returns, img_path
@@ -89,11 +73,8 @@ def predictFromTensor(tensor):
         }
     }
 
-    # print(image.shape)
-
     for mod in models.keys():
         labels = get_label(mod, models[mod], image)
-        # print(labels[0], f"{labels[1] * 100}%")
         returns[mod]["class"], returns[mod]["confidence"] = labels[0], f"{labels[1] * 100}%"
 
     return returns
