d253d0312524f659eb3fa598a39ac5031509fc1f
22preich <44716533+22preich@users.noreply.github.com>
2020-05-27 19:29:14 -0700

    gunicorn

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python main.py
+web: gunicorn -w 2 main:app
--- main.py
+++ main.py
@@ -39,4 +39,4 @@ def file_uploaded():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, port=80)
